7f9cedde8478f614b51ff02892715d018af6a83a
NaimDikki <naimdikki@gmail.com>
2024-07-08 18:07:47 +0200

    added ec2 addres instead of frontend address

--- myproject/settings.py
+++ myproject/settings.py
@@ -180,10 +180,10 @@ AUTH_USER_MODEL = 'signing.Client'
 
 # cors origin
 CORS_ALLOW_ALL_ORIGINS = True
- CORS_ALLOWED_ORIGINS = [
-    '*'  # my React frontend origin
+CORS_ALLOWED_ORIGINS = [
+    'http://100.26.213.54'  # my React frontend origin
     
-] 
+]
 
 CORS_ALLOW_HEADERS = ['Content-Type', 'X-CSRFToken', 'Authorization', 'X-Product-ID', 'product_id', 'csrfmiddlewaretoken', 'csrftoken']
 
@@ -205,12 +205,4 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'react-ui/build/static')
-]
-
-
-CORS_ORIGIN_ALLOW_ALL=True
-
-
-CORS_ORIGIN_WHITELIST = [
-    '*',
 ]
